a46b7e1111274ba2fca54bcaca24ef6deddeb21a
AndMac <chang.crazy@hotmail.com>
2020-10-02 23:34:52 +0200

    数据库配置

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'itheima_blog',
+        'USER': 'itheima',
+        'PASSWORD': '123456',
+        'HOST': '127.0.0.1',   # Or an IP Address that your DB is hosted on
+        'PORT': 3306,
     }
 }
 
